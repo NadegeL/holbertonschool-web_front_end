64c8ad34cb9b8bb7f7053ba97fa5526b240ed284
NadegeL <nadege.luthier@gmai.com>
2024-11-11 16:47:14 +0100

    learn short syntax

---
+++ ES6_basic/5-main.js
@@ -0,0 +1,6 @@
+/*Using spread syntax, concatenate 2 arrays and each character of
+ a string by modifying the function below. Your function body should be one line long.*/
+
+ import concatArrays from './5-spread-operator.js';
+
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
---
+++ ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,4 @@
+/*Modify the following function’s budget object to simply use the object property value shorthand syntax instead.*/
+export default function getBudgetObject(income, gdp, capita) {
+    return {income, gdp, capita};
+  }
