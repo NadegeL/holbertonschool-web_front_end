d365808aaa24d701b1d49a35715261046dfe9d93
NadegeL <nadege.luthier@gmai.com>
2024-11-12 15:36:11 +0100

    migration de répot

--- ES6_basic/10-loops.js
+++
@@ -1,7 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-    for (let idx in array) {
-      array[idx] = appendString + array[idx];
-    }
-  
-    return array;
-  }
--- ES6_basic/5-main.js
+++
@@ -1,6 +0,0 @@
-/*Using spread syntax, concatenate 2 arrays and each character of
- a string by modifying the function below. Your function body should be one line long.*/
-
- import concatArrays from './5-spread-operator.js';
-
-console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
